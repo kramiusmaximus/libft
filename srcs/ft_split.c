23d985d64865088d53d9ec8a4bba81161c141e36
kram <markartyukh@gmail.com>
2020-12-22 16:39:20 +0300

    changed ""#include "../includes/libft.h" to #include "libft.h"

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
+#include "libft.h"
 
 static void		*ft_execute_order_66(char **buff, int len)
 {
--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
+#include "libft.h"
 
 int	ft_strlen(const char *str)
 {
